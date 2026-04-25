c3743f6c2072f31c1510f66a9df20a4aa252c9fa
Jcvanz-wehsoft <julio.vanz@wehsoft.com>
2026-04-25 18:31:09 -0300

    fix: database seeder and vite build for production

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,24 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'name' => 'Admin',
-            'email' => 'email@email.com',
-            'password' => 'password'      
+            'name' => 'Julio Cesar Vanz',
+            'email' => 'julio@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        \App\Models\Portfolio::create([
+            'name' => 'Julio Cesar Vanz',
+            'bio' => 'Desenvolvedor Fullstack',
+            'email' => 'julio@example.com',
+            'github' => 'https://github.com/Jcvanz',
+            'linkedin' => 'https://linkedin.com/in/juliocesarvanz',
+            'city' => 'Blumenau',
+            'state' => 'SC'
+        ]);
+
+        \App\Models\CoreStack::create([
+            'name' => 'Laravel',
+            'icon' => 'https://raw.githubusercontent.com/devicons/devicon/master/icons/laravel/laravel-original.svg'
         ]);
     }
 }
