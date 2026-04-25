4faa77e6e6aa588127e8e75f35fbf358136f6a3e
Jcvanz-wehsoft <julio.vanz@wehsoft.com>
2026-04-25 17:18:07 -0300

    feat: final adjustments, premium readme, favicon and portfolio dynamics

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portifolio Julio Cesar</title>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/particles.js'])
 </head>
 <body class="bg-slate-950">
