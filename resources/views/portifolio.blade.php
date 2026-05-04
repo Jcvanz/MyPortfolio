fcc222dd032cfa8b603afefcb33146620cc808bd
Jcvanz-wehsoft <julio.vanz@wehsoft.com>
2026-05-04 11:28:20 -0300

    Criado exibição das experiências no arquivo principal

--- resources/views/portifolio.blade.php
+++ resources/views/portifolio.blade.php
@@ -6,10 +6,12 @@
         <div class="absolute inset-0 bg-[linear-gradient(rgba(255,255,255,0.02)_1px,transparent_1px),linear-gradient(90deg,rgba(255,255,255,0.02)_1px,transparent_1px)] bg-size-[40px_40px] mask-[radial-gradient(ellipse_60%_60%_at_50%_50%,#000_10%,transparent_100%)] pointer-events-none"></div>
 
         <x-portfolio.hero :portfolio="$portfolio" />
-        
-        <x-portfolio.core-stacks :portfolio="$portfolio" :coreStacks="$coreStacks" />
 
+        <x-portfolio.core-stacks :portfolio="$portfolio" :coreStacks="$coreStacks" />
+        
         <x-portfolio.skill-radar :portfolio="$portfolio" />
+
+        <x-portfolio.experience :experiences="$experiences" />
 
         <x-portfolio.github-matrix :portfolio="$portfolio" />
 
