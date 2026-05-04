a7276e5d089f269c4e98c7f3bcfc1cef820deacc
Jcvanz-wehsoft <julio.vanz@wehsoft.com>
2026-05-04 19:14:16 -0300

    Ajuste na escrita do titulo do site

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portifolio Julio Cesar</title>
+    <title>Portfolio Julio Cesar</title>
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/particles.js'])
 </head>
--- resources/views/layouts/layout_admin.blade.php
+++ resources/views/layouts/layout_admin.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portifolio - Admin</title>
+    <title>Portfolio - Admin</title>
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     @vite(['resources/css/app.css', 'resources/js/admin.js'])
 
